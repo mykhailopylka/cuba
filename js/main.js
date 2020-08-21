df1851a15e1f5e8623a641b3a6b625383f6d4fd5
origin <mykhailo.pylka@gmail.com>
2020-08-21 15:15:50 +0300

    redirect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(function(){
+// $(function () {
+//   $(".photo-slider").slick({
+//     arrows: true,
+//     prevArrow:
+//       '<button type="button" class="slick-prev"><img src="images/arrow_l.svg" alt="arrow"></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"><img src="images/arrow_r.svg" alt="arrow"></button>',
+//   });
+// });
 
+$(document).ready(function () {
+  $(".photo-slider").slick({
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow_l.svg" alt="arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow_r.svg" alt="arrow"></button>',
+  });
+});
+
+$(document).ready(function () {
+  $(".test-slider").slick({
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow_l.svg" alt="arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow_r.svg" alt="arrow"></button>',
+  });
 });
